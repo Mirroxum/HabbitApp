55482801a4012ab28816c24a82d70b6e6c45b961
mirrox <igor.saraev@fishplace.ru>
2024-03-19 18:01:57 +1000

    render page

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,20 @@
 
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY'
+let globalActiveHabbitId
 
 //page
 const page = {
-	menu: document.querySelector('.menu__list')
+	menu: document.querySelector('.menu__list'),
+	header: {
+		h1: document.querySelector('.h1'),
+		progressPercent: document.querySelector('.progres__percent'),
+		progressCoverBar: document.querySelector('.progres__cover-bar'),
+	},
+	content: {
+		daysContainer: document.getElementById('days'),
+		nextDay: document.querySelector('.habbit__day')
+	},
 }
 
 // utils
@@ -18,24 +28,22 @@ function loadData() {
 	}
 }
 
-function saveDate() {
+function saveData() {
 	localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
 }
 
 //renderfunc
 function rerenderMenu(activeHabbit) {
-	if (!activeHabbit) {
-		return
-	}
 	for (const habbit of habbits) {
 		const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
 		if (!existed) {
 			const element = document.createElement('button');
 			element.setAttribute('menu-habbit-id', habbit.id);
 			element.classList.add('menu__item');
-			element.innerHTML = `<img src="/images/${habbit.icon}" alt="${habbit.name}">`;
+			element.addEventListener('click', () => rerender(habbit.id))
+			element.innerHTML = `<img src="/images/${habbit.icon}.svg" alt="${habbit.name}">`;
 			if (activeHabbit.id === habbit.id) {
-				existed.classList.add('menu__item_active');
+				element.classList.add('menu__item_active');
 			}
 			page.menu.appendChild(element);
 			continue;
@@ -48,10 +56,66 @@ function rerenderMenu(activeHabbit) {
 	}
 }
 
-function rerender(activeHabbitId) {
-	const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
-	rerenderMenu(activeHabbit)
+function rerenderHead(activeHabbit) {
+	page.header.h1.innerText = activeHabbit.name;
+	const progress = activeHabbit.days.length / activeHabbit.target > 1 
+		? 100
+		: activeHabbit.days.length / activeHabbit.target * 100;
+	page.header.progressPercent.innerText = `${progress.toFixed(0)}%`;
+	page.header.progressCoverBar.setAttribute('style', `width: ${progress}%;`)
 }
+
+function rerenderContent(activeHabbit) {
+	page.content.daysContainer.innerHTML = '';
+	for (const index in activeHabbit.days) {
+		const element = document.createElement('div');
+		element.classList.add('habbit');
+		element.innerHTML = `<div class="habbit__day">День ${Number(index) + 1}</div>
+            <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
+            <button class="habbit__delete">
+              <img src="images/delete.svg" alt="Удалить день ${Number(index) + 1}">
+            </button>`;
+		page.content.daysContainer.appendChild(element);
+		page.content.nextDay.innerText = `День ${activeHabbit.days.length + 1}`;
+	}
+
+}
+
+function rerender(activeHabbitId) {
+	globalActiveHabbitId = activeHabbitId
+	const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
+	if (!activeHabbit) {
+		return;
+	}
+	rerenderMenu(activeHabbit);
+	rerenderHead(activeHabbit);
+	rerenderContent(activeHabbit);
+}
+
+// work with days
+function addDays(event) {
+	event.preventDefault();
+	const form = event.target
+	const data = new FormData(form);
+	const comment = data.get('comment');
+	form['comment'].classList.remove('error');
+	if (!comment) {
+		form['comment'].classList.add('error');
+	}
+	habbits = habbits.map((habbit) => {
+		if (habbit.id === globalActiveHabbitId && comment) {
+			return {
+				...habbit,
+				days: habbit.days.concat([{comment}])
+			}
+		}
+		return habbit;
+	})
+	form['comment'].value = '';
+	rerender(globalActiveHabbitId)
+	saveData();
+}
+
 
 // innit
 
